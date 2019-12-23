4660d1c775815dde66a5ca3719d0880a50e45a19
Marcio Sousa <marciofrapeso@gmail.com>
2019-12-23 14:24:16 -0300

    04 - Associação no Entity Framework.

--- Projeto01/Projeto01/Models/Fabricante.cs
+++ Projeto01/Projeto01/Models/Fabricante.cs
@@ -1,8 +1,12 @@
-﻿namespace Projeto01.Models
+﻿using System.Collections.Generic;
+
+namespace Projeto01.Models
 {
     public class Fabricante
     {
         public long FabricanteId { get; set; }
         public string Nome { get; set; }
+
+        public virtual ICollection<Produto> Produtos { get; set; }
     }
 }
